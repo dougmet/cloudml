5836e4168f0d33246f04e3edb274ee5bac556f26
Douglas Ashton <douglas.j.ashton@gmail.com>
2019-04-08 13:36:21 +0100

    remove git-bash extras from submit.sh

--- python/iris/submit.sh
+++ python/iris/submit.sh
@@ -1,12 +1,15 @@
-gcloud.cmd ml-engine jobs submit training pytest000002 \
+# Submit from inside the iris directory (where this sh file is)
+
+# Submit to ML Engine
+gcloud ml-engine jobs submit training pytest000002 \
     --module-name iris.iris_train \
     --region us-central1 \
     --package-path iris \
     --python-version 3.5 \
     --job-dir gs://keras-235720/py-cloudml
 
-# Run from inside the iris directory
-gcloud.cmd ml-engine local train \
+# Local Run
+gcloud ml-engine local train \
     --module-name iris.iris_train \
     --package-path iris \
     --job-dir output
